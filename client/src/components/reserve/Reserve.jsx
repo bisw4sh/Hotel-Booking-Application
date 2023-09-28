ab898cef38de4a8c777adec4c8a76f14a59bba1d
Yaman8 <yaman.subedi@nimblebees.com>
2023-09-28 23:18:35 +0545

    fixed

--- client/src/components/reserve/Reserve.jsx
+++ client/src/components/reserve/Reserve.jsx
@@ -55,9 +55,12 @@ const Reserve = ({ setOpen, hotelId }) => {
     try {
       await Promise.all(
         selectedRooms.map((roomId) => {
-          const res = axios.put(`/rooms/availability/${roomId}`, {
-            dates: alldates,
-          });
+          const res = axios.put(
+            `http://localhost:8800/api/rooms/availability/${roomId}`,
+            {
+              dates: alldates,
+            }
+          );
           return res.data;
         })
       );
